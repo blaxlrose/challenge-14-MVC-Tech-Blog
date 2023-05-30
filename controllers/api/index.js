daba4ac4364e9f3a92de016dcc8fbea4c1cbfd6c
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-05-29 23:09:29 -0400

    schema and index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 
+router.use('/api', userRoutes);
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
 
